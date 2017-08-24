3038910619924f1427f45dd9b526e46165804a6a
Sidney Nguyen <imsidthesquid@gmail.com>
2017-08-24 10:55:39 -0700

    Created a JavaFX GUI to test the library

--- src/com/sidneynguyen/otjava/Main.java
+++ src/com/sidneynguyen/otjava/Main.java
@@ -1,10 +1,23 @@
 package com.sidneynguyen.otjava;
 
-import java.util.UUID;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, World!");
+	    launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("tester.fxml"));
+        primaryStage.setTitle("Tester");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
 }
---
+++ src/com/sidneynguyen/otjava/TesterController.java
@@ -0,0 +1,92 @@
+package com.sidneynguyen.otjava;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.util.UUID;
+
+// TODO: Fix bugs regarding multiple pushes and pulls during edits
+public class TesterController {
+    private ClientDriver clientDriver1;
+    private ClientDriver clientDriver2;
+    private ServerDriver serverDriver;
+
+    @FXML
+    private Label serverDocumentLabel;
+
+    @FXML
+    private TextArea client1TextArea;
+
+    @FXML
+    private TextArea client2TextArea;
+
+    public TesterController() {
+        String key = UUID.randomUUID().toString();
+        String originalDocument = "Hello";
+        clientDriver1 = new ClientDriver(originalDocument, key);
+        clientDriver2 = new ClientDriver(originalDocument, key);
+        serverDriver = new ServerDriver(originalDocument, key);
+    }
+
+    @FXML
+    protected void handleClient1PushButtonAction(ActionEvent event) {
+        String text = client1TextArea.getText();
+        if (text.equals(clientDriver1.getDocument().getData())) {
+            return;
+        }
+
+        Operation operation = clientDriver1.getOperationFromEdit(text);
+        clientDriver1.enqueueClientOperation(operation);
+        clientDriver1.processChange();
+        client1TextArea.setText(clientDriver1.getDocument().getData());
+
+        ServerOperation clientOperation = clientDriver1.sendClientOperationToServer();
+        serverDriver.enqueueClientOperation(clientOperation);
+        serverDriver.processChange();
+
+        serverDocumentLabel.setText(serverDriver.getDocument().getData());
+
+        ServerOperation serverOperation = serverDriver.sendServerOperationToClient();
+        clientDriver1.enqueueServerOperation(serverOperation);
+        clientDriver1.processChange();
+        clientDriver2.enqueueServerOperation(serverOperation);
+        clientDriver2.processChange();
+    }
+
+    @FXML
+    protected void handleClient2PushButtonAction(ActionEvent event) {
+        String text = client2TextArea.getText();
+        if (text.equals(clientDriver2.getDocument().getData())) {
+            return;
+        }
+
+        Operation operation = clientDriver2.getOperationFromEdit(text);
+        clientDriver2.enqueueClientOperation(operation);
+        clientDriver2.processChange();
+        client2TextArea.setText(clientDriver2.getDocument().getData());
+
+        ServerOperation clientOperation = clientDriver2.sendClientOperationToServer();
+        serverDriver.enqueueClientOperation(clientOperation);
+        serverDriver.processChange();
+
+        serverDocumentLabel.setText(serverDriver.getDocument().getData());
+
+        ServerOperation serverOperation = serverDriver.sendServerOperationToClient();
+        clientDriver1.enqueueServerOperation(serverOperation);
+        clientDriver1.processChange();
+        clientDriver2.enqueueServerOperation(serverOperation);
+        clientDriver2.processChange();
+    }
+
+    @FXML
+    protected void handleClient1PullButtonAction(ActionEvent event) {
+        client1TextArea.setText(clientDriver1.getDocument().getData());
+    }
+
+    @FXML
+    protected void handleClient2PullButtonAction(ActionEvent event) {
+        client2TextArea.setText(clientDriver2.getDocument().getData());
+    }
+}
